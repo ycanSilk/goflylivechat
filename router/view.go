96a05e8258d9effdc96b4efd30361aa855c1dd15
taoshihan <golivechat@outlook.com>
2025-08-29 14:58:12 +0800

    fix

--- router/view.go
+++ router/view.go
@@ -17,9 +17,6 @@ func InitViewRouter(engine *gin.Engine) {
 	engine.GET("/chat_main", middleware.JwtPageMiddleware, middleware.DomainLimitMiddleware, tmpl.PageChatMain)
 	engine.GET("/setting", middleware.DomainLimitMiddleware, tmpl.PageSetting)
 	engine.GET("/setting_statistics", tmpl.PageSettingStatis)
-	engine.GET("/setting_indexpage", tmpl.PageSettingIndexPage)
-	engine.GET("/setting_indexpages", tmpl.PageSettingIndexPages)
-	engine.GET("/setting_mysql", tmpl.PageSettingMysql)
 	engine.GET("/setting_welcome", tmpl.PageSettingWelcome)
 	engine.GET("/setting_deploy", tmpl.PageSettingDeploy)
 	engine.GET("/setting_kefu_list", tmpl.PageKefuList)
@@ -27,6 +24,4 @@ func InitViewRouter(engine *gin.Engine) {
 	engine.GET("/setting_modifypass", tmpl.PageModifypass)
 	engine.GET("/setting_ipblack", tmpl.PageIpblack)
 	engine.GET("/setting_config", tmpl.PageConfig)
-	engine.GET("/mail_list", tmpl.PageMailList)
-	engine.GET("/roles_list", tmpl.PageRoleList)
 }
